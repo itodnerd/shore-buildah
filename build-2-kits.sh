c67ba10254eaee971558329be036e5a094f7b771
Demian Vöhringer <demian.voehringer@fau.de>
2021-11-02 14:33:27 +0100

    fail if one fails

--- build-2-kits.sh
+++ build-2-kits.sh
@@ -2,13 +2,12 @@
 
 ctr=$(buildah from shore-mt)
 buildah config --env GOPATH=/home/git $ctr
-buildah run $ctr /bin/sh -c 'cd /home/git;\
-    git clone https://github.com/itodnerd/shore-kits.git kits; \
-    cd kits;\
-    ln -s /home/git/mt/m4;\
-    ls;\
+buildah run $ctr /bin/sh -c 'cd /home/git &&
+    git clone https://github.com/itodnerd/shore-kits.git kits &&
+    cd kits &&
+    ln -s /home/git/mt/m4 &&
     ./autogen.sh &&
     ./configure --with-shore=/home/git/mt --with-glibtop --enable-debug &&
-    make -j;'
+    make -j;' &&
 
 buildah commit $ctr shore-kit
