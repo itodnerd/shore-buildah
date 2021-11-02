54f98ce62d629dad39cd0d5fd60c1fc39590941a
Demian Vöhringer <demian.voehringer@fau.de>
2021-11-02 15:03:56 +0100

    switch to extra config branch

--- build-2-kits.sh
+++ build-2-kits.sh
@@ -5,6 +5,7 @@ buildah config --env GOPATH=/home/git $ctr
 buildah run $ctr /bin/sh -c 'cd /home/git &&
     git clone https://github.com/itodnerd/shore-kits.git kits &&
     cd kits &&
+    git checkout config &&
     ln -s /home/git/mt/m4 &&
     ./autogen.sh &&
     ./configure --with-shore=/home/git/mt --with-glibtop --enable-debug &&
